10607f71418dc46a2a12700e756c1a2f5f862fd1
aalmonacidd <live@slackware.example.org>
2021-11-28 18:17:01 -0500

    Tarea tiempo de multiplicacion de matrices

--- tareas/tiempomultmatriz/tiempo.cpp
+++ tareas/tiempomultmatriz/tiempo.cpp
@@ -7,35 +7,37 @@
 #include <algorithm>
 
 void multiply(const std::vector<double> & m1, const std::vector<double> & m2, std::vector<double> & m3);
+double sumaterminos(const std::vector<double> & m1, const std::vector<double> & m2, int i, int j, int N);
 
 int main(int argc, char **argv) {
-  // read parameters
-  const int N = std::atoi(argv[1]);
-  const int SEED = std::atoi(argv[2]);
+    const int SEED = std::atoi(argv[1]);
+    double durcuatro=1;//inicializar variable de duracion para matrices 4*4
+    for(int k=2;k<=9;k++){
+        int N=pow(2,k);
+        std::vector<double> A(N*N, 0.0), B(N*N, 0.0), C(N*N, 0.0);
+        std::mt19937 gen(SEED);
+        std::uniform_real_distribution<> dist(0.,1.);
+        // lambda function: a local function that captures [] something, receives something (), and return something {}
+        // See: https://www.learncpp.com/cpp-tutorial/introduction-to-lambdas-anonymous-functions/
+        std::generate(A.begin(), A.end(), [&gen, &dist](){ return dist(gen); }); // uses a lambda function
+        std::generate(B.begin(), B.end(), [&gen, &dist](){ return dist(gen); }); // uses a lambda function
 
-  // data structs
-  std::vector<double> A(N*N, 0.0), B(N*N, 0.0), C(N*N, 0.0);
+      // multiply the matrices A and B and save the result into C. Measure time
+      auto start = std::chrono::high_resolution_clock::now();
+      multiply(A, B, C);
+      auto stop = std::chrono::high_resolution_clock::now();
 
-  // fill matrices A and B, using random numbers betwwen 0 and 1
-  std::mt19937 gen(SEED);
-  std::uniform_real_distribution<> dist(0.,1.);
-  // lambda function: a local function that captures [] something, receives something (), and return something {}
-  // See: https://www.learncpp.com/cpp-tutorial/introduction-to-lambdas-anonymous-functions/
-  std::generate(A.begin(), A.end(), [&gen, &dist](){ return dist(gen); }); // uses a lambda function
-  std::generate(B.begin(), B.end(), [&gen, &dist](){ return dist(gen); }); // uses a lambda function
+      // use the matrix to avoid the compiler removing it
+      C[N/2];
+      std::cout<<"Tiempo para N="<<N<<std::endl;
 
-  // multiply the matrices A and B and save the result into C. Measure time
-  auto start = std::chrono::high_resolution_clock::now();
-  multiply(A, B, C);
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  // use the matrix to avoid the compiler removing it
-  std::cout << C[N/2] << std::endl;
-
-  // print time
-  auto elapsed = std::chrono::duration<double>(stop - start);
-  std::cout << elapsed.count() << "\n";
-
+      // print time
+      auto elapsed = std::chrono::duration<double>(stop - start);
+      if(k==2){
+          durcuatro=elapsed.count();
+      }
+      std::cout << elapsed.count()/durcuatro << "\n\n";
+}
   return 0;
 }
 
@@ -43,5 +45,17 @@ int main(int argc, char **argv) {
 void multiply(const std::vector<double> & m1, const std::vector<double> & m2, std::vector<double> & m3)
 {
   const int N = std::sqrt(m1.size()); // assumes square matrices
-  delete this line and implement the matrix multiplication here
+  for(int o=0;o<N;o++){
+      for(int p=0;p<N;p++){
+          m3[o*N+p]=sumaterminos(m1,m2,o,p,N);
+      }
+  }
+}
+
+double sumaterminos(const std::vector<double> & m1, const std::vector<double> & m2, int i, int j, int N){
+    double suma=0;
+    for(int k=0;k<N;k++){
+        suma=m1[i*N+k]*m2[k*N+j]; //indices de dos dimensiones a vector 1d
+    }
+    return suma;
 }
